21a9d4590db6854b77e27fffe3a9dbe343b492f3
DESKTOP-G7JTIU0\victor <vmdlssnt@gmail.com>
2018-04-19 09:55:08 -0800

    fifth attempt

--- controllers/PollController.js
+++ controllers/PollController.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 const PollObject = require('../models/poll')
 const OptionObject = require('../models/option')
 const jwt = require('jwt-simple')
+var env = process.env.NODE_ENV||'dev'
 if (env=='dev') {
 	var config = require('../config')
 }
